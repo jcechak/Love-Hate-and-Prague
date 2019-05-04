0d0a48effcbd73dffcc865bb927d47297fa8c492
Jiří Mauritz <jirmauritz@gmail.com>
2019-05-04 20:02:48 +0200

    Refining geocoding

--- location.py
+++ location.py
@@ -4,9 +4,10 @@ import os
 
 def get_location(text):
     google_api_key = os.environ['GOOGLE_MAPS_API_KEY']
-    gmaps = GooglePlaces(google_api_key);
-    jsonresult = gmaps.text_search(text + " ,Praha", lat_lng= {'lat':50.075538, 'lng':14.437800}, radius=50000)
-    for item in jsonresult.places:
-        print(item)
-    return jsonresult.places
-
+    gmaps = GooglePlaces(google_api_key)
+    jsonresult = gmaps.text_search(text + " ,Praha", lat_lng={'lat': 50.075538, 'lng': 14.437800}, radius=50000)
+    jsonresult.places.reverse()
+    place = jsonresult.places.pop()
+    lat = place.geo_location.get('lat')
+    lng = place.geo_location.get('lng')
+    return {'lat': float(lat), 'lng': float(lng)}
